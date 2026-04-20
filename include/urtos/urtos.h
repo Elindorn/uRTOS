a47ac5392de404dc3e4f157d72199c7210ea15fc
Elindorn <maximemeljanoff@gmail.com>
2026-04-20 22:54:02 +0300

    Header documentation

--- include/urtos/urtos.h
+++ include/urtos/urtos.h
@@ -1,3 +1,17 @@
+/**
+ * @file urtos.h
+ * @brief Public API for uRTOS - a lightweight real-time operating system for AVR.
+ * 
+ * uRTOS provides a preemptive/cooperative hybrid scheduler with static task
+ * definitions stored in PROGMEM. It designed for simplicity and minimal
+ * overhead on ATmega328 and similar AVR MCUs.
+ * 
+ * @author Elindorn
+ * @copyright Copyright (c) 2026 Elindorn
+ * @licence MIT Licence
+ */
+
+
 #ifndef __uRTOS_HEADER__
 #define __uRTOS_HEADER__
 
@@ -6,19 +20,69 @@
 #include <stdint.h>
 
 
+/**
+ * @defgroup public_api Public API
+ * @brief Macros, types and functions available to the application.
+ * @{
+ */
+
+/**
+ * @def uRTOS_PROC_HANDLE(name)
+ * @brief Declare a task function.
+ * 
+ * Task function must never return. The macro adds the necessary attributes
+ * to prevent compiler optimizations that could break context switching.
+ * 
+ * @param name Task function name. 
+ */
 #define uRTOS_PROC_HANDLE(name) void __attribute__((used, noinline, noreturn)) name(void)
+
+/**
+ * @def uRTOS_SCHEDULER(name)
+ * @brief Declare a custom scheduler function.
+ * 
+ * Scheduler functions are called from the tick handler on the system stack.
+ * They must be declared with some attributes to ensure correct code generation.
+ * 
+ * @param name Scheduler function name.
+ */
 #define uRTOS_SCHEDULER(name) void __attribute__((used, noinline)) name(void)
 
+/**
+ * @def uRTOS_INSTALL(vector)
+ * @brief Install the uRTOS tick for a given timer overflow interrupt.
+ * 
+ * This macro creates ISR that jumps to `uRTOS_TickHandler`.
+ * It must be placed in the application code.
+ * 
+ * @param vector AVR interrupt vector name (e.g., TIMER2_OVF_vect).
+ */
 #define uRTOS_INSTALL(vector) ISR(vector, ISR_NAKED) { __asm__ __volatile__ ("jmp uRTOS_TickHandler" ::); }
 
-#define uRTOS_TFLAGS_NONE		0
-#define uRTOS_TFLAGS_SUSPENDED	1
+/**
+ * @defgroup task_flags Task flags
+ * @{
+ */
+#define uRTOS_TFLAGS_NONE		0 /**< No special flags. */
+#define uRTOS_TFLAGS_SUSPENDED	1 /**< Task is suspended and will not be scheduled. */
+/** @} */ // end of task_flags
 
-#define uRTOS_TIM_PRESC_1		0b001
-#define uRTOS_TIM_PRESC_8		0b010
-#define uRTOS_TIM_PRESC_64		0b011
-#define uRTOS_TIM_PRESC_256		0b100
-#define uRTOS_TIM_PRESC_1024	0b101
+/**
+ * @defgroup timer_presc Timer prescaler values
+ * 
+ * Tick frequency is calculated by `CPU_clk/TIM_prescaler/TIM_bitlength`.
+ * See your MCU's datasheet.
+ * 
+ * @{
+ */
+#define uRTOS_TIM_PRESC_1		0b001 /**< clk/1 */
+#define uRTOS_TIM_PRESC_8		0b010 /**< clk/8 */
+#define uRTOS_TIM_PRESC_64		0b011 /**< clk/64 */
+#define uRTOS_TIM_PRESC_256		0b100 /**< clk/256 */
+#define uRTOS_TIM_PRESC_1024	0b101 /**< clk/1024 */
+/** @} */ // end of timer_presc
+
+/** @} */ // end of public_api
 
 
 #ifdef __cplusplus
@@ -26,25 +90,62 @@ extern "C" {
 #endif
 
 
+/**
+ * @brief Function pointer type for a scheduler.
+ * 
+ * Scheduler functions are called from the tick handler on the system stack.
+ * They must update `__uRTOS_STATIC_INFO_PTR->current` to point to the next
+ * runnable TCB.
+ */
 typedef void (*Scheduler_t)(void);
+
+/**
+ * @brief Function pointer type for a task procedure. 
+ */
 typedef void (*ProcAddr_t)(void);
 
-
+/**
+ * @brief Generic pointer type
+ */
 typedef void* Pointer_t;
+
+/**
+ * @brief Pointer to the saved stack pointer of a task.
+ */
 typedef Pointer_t StackPtr_t;
+
+/**
+ * @brief Pointer to the base of a task's stack.
+ */
 typedef Pointer_t BasePtr_t;
 
+/**
+ * @brief Task flags bitfield.
+ */
 typedef uint8_t TaskFlags_t;
 
+/**
+ * @brief Task identifier (index in the task array).
+ */
 typedef size_t TaskId_t;
 
+/**
+ * @brief System initialization information (should be stored in PROGMEM).
+ * 
+ * Passed to uRTOS_Run() to configure the system.
+ */
 typedef struct _SystemInitInfoStruct
 {
-	uint8_t timerNo;
-	uint8_t timerPrescaler;
-	Scheduler_t scheduler;
+	uint8_t timerNo;			/**< Timer number (0, 1, or 2). */
+	uint8_t timerPrescaler;		/**< Timer prescaler (use uRTOS_TIM_PRESC_*). */
+	Scheduler_t scheduler;		/**< Pinter to the scheduler function. */
 } SysInitInfo_t;
 
+/**
+ * @brief Task descriptor (should be stored in PROGMEM).
+ * 
+ * Defines a task's entry point, stack size, and initial flags.
+ */
 typedef struct _TaskDescriptorStruct
 {
 	ProcAddr_t handle;
@@ -52,17 +153,67 @@ typedef struct _TaskDescriptorStruct
 	TaskFlags_t flags;
 } TaskDesc_t;
 
-
+/**
+ * @brief Start the uRTOS kernel.
+ * 
+ * This function will never return. It switches to the system stack, initializes
+ * internal data structures, and launches the first runnable task.
+ * 
+ * @param initInfo Pointer to system initialization info (in PROGMEM).
+ * @param tasks Pointer to array of task descriptors (in PROGMEM).
+ * @param nTasks Number of tasks in the array.
+ */
 void uRTOS_Run(const SysInitInfo_t* initInfo, const TaskDesc_t* tasks, size_t nTasks) __attribute__((noreturn));
 
+/**
+ * @brief Yield the processor to the next runnable task.
+ * 
+ * Can be called from a task to voluntarily give up the processor. It forces
+ * an immediate timer overflow interrupt, which triggers the scheduler.
+ */
 void uRTOS_Yield(void);
 
+/**
+ * @brief Suspend a task.
+ * 
+ * The suspended task will not be scheduled until re-enabled.
+ * 
+ * @param id Task identifier (0..nTasks-1).
+ */
 void uRTOS_DisableTask(TaskId_t id);
+
+/**
+ * @brief Suspend the calling task.
+ * 
+ * The current task is suspended and the scheduler is invoked immediately.
+ * The function does not return until the task is re-enabled and scheduled again.
+ */
 void uRTOS_DisableCurrentTask(void);
+
+/**
+ * @brief Resume a suspended task.
+ * 
+ * @param id Task identifier (0..nTasks-1).
+ */
 void uRTOS_EnableTask(TaskId_t id);
 
+/**
+ * @brief Get the current system tick count.
+ * 
+ * The tick counter is incremented in the timer ISR. This counter overflows
+ * after 50.9 days since kernal start (F_CPU=16MHz, PRESC=256, 8-bit timer).
+ * 
+ * @return Number of ticks since the kernel started.
+ */
 unsigned long uRTOS_GetTick();
 
+/**
+ * @brief Default round-robin scheduler.
+ * 
+ * This scheduler selects the next task in a circular manner, skipping any
+ * tasks with non-zero flags (e.g., suspended). It will hangs forever if all
+ * tasks is suspended.
+ */
 uRTOS_SCHEDULER(uRTOS_Sched_RoundRobin);
 
 
--- include/urtos/urtos_internal.h
+++ include/urtos/urtos_internal.h
@@ -1,3 +1,17 @@
+/**
+ * @file urtos_internal.h
+ * @brief Internal definitions for uRTOS.
+ * 
+ * This header is **not** part of the public API. It contains hardware-specific
+ * macros and stuctures used by the kernel implementation. Applications
+ * should not include this file directly.
+ * 
+ * @author Elindorn
+ * @copyright Copyright (c) 2026 Elindorn
+ * @licence MIT Licence
+ */
+
+
 #ifndef __uRTOS_INTERNAL_HEADER__
 #define __uRTOS_INTERNAL_HEADER__
 
@@ -7,29 +21,46 @@
 #include <urtos/urtos.h>
 
 
+/**
+ * @defgroup mem_layout Memory layout constants
+ * @{
+ */
+/** Null pointers representations. */
 #define __uRTOS_NULLPTR ((void*)0x0000)
+/** Top of RAM. */
 #define __uRTOS_RAM_END ((Pointer_t)(RAMEND))
-
+/** Address of the StaticInfo_t structure (placed at the very top of RAM). */
 #define __uRTOS_STATIC_INFO_PTR ((StaticInfo_t*)(RAMEND - sizeof(StaticInfo_t) + 1))
-
+/** Starting address of the system stack (grows downward). */
 #define __uRTOS_SYSTEM_STACK_START ((void*)(__uRTOS_RAM_END - sizeof(StaticInfo_t)))
-
+/** Size of the system stack in bytes. */
 #define __uRTOS_SYSTEM_STACK_SIZE 64
+/** @} */ // end of mem_layout
 
 
-#define __uRTOS_TCCRA(timer)	TCCR ## timer ## A
-#define __uRTOS_TCCRB(timer)	TCCR ## timer ## B
-#define __uRTOS_TIMSK(timer)	TIMSK ## timer
-#define __uRTOS_TIFR(timer)		TIFR ## timer
-#define __uRTOS_TCNT(timer)		TCNT ## timer
-#define __uRTOS_TCNT_H(timer)	TCNT ## timer ## H
-#define __uRTOS_TCNT_L(timer)	TCNT ## timer ## L
+/**
+ * @defgroup tim_regs Timer register access macros
+ * @{
+ */
+#define __uRTOS_TCCRA(timer)	TCCR ## timer ## A		/**< Timer/Counter Control Register A */
+#define __uRTOS_TCCRB(timer)	TCCR ## timer ## B		/**< Timer/Counter Control Register B */
+#define __uRTOS_TIMSK(timer)	TIMSK ## timer			/**< Timer Interrupt Mask Register */
+#define __uRTOS_TIFR(timer)		TIFR ## timer			/**< Timer Interrupt Flag Register */
+#define __uRTOS_TCNT(timer)		TCNT ## timer			/**< Timer Counter (8-bit) */
+#define __uRTOS_TCNT_H(timer)	TCNT ## timer ## H		/**< Timer Counter High byte (16-bit) */
+#define __uRTOS_TCNT_L(timer)	TCNT ## timer ## L		/**< Timer Counter Low byte (16-bit) */
+/** @} */ // end of tim_regs
 
-#define __uRTOS_TIM_OFFSET	0
-#define __uRTOS_TIM_MASK	(0b11 << __uRTOS_TIM_OFFSET)
-#define __uRTOS_TIM0		0b00
-#define __uRTOS_TIM1		0b01
-#define __uRTOS_TIM2		0b10
+/**
+ * @defgroup tim_id_flags Timer identifications bits (stored in sysFlags)
+ * @{
+ */
+#define __uRTOS_TIM_OFFSET	0		/**< Bit offset of timer ID in sysFlags. */
+#define __uRTOS_TIM_MASK	(0b11 << __uRTOS_TIM_OFFSET) /**< Mask for timer ID bits. */
+#define __uRTOS_TIM0		0b00	/**< Timer 0 selected. */
+#define __uRTOS_TIM1		0b01	/**< Timer 1 selected. */
+#define __uRTOS_TIM2		0b10	/**< Timer 2 selected. */
+/** @} */ // end of tim_id_flags
 
 
 #ifdef __cplusplus
@@ -37,34 +68,64 @@ extern "C" {
 #endif
 
 
+/**
+ * @brief Task Control Block (TCB).
+ * 
+ * Holds the saved stack pointer, stack base, and status flags for one task.
+ */
 typedef struct _TaskControlBlockStruct
 {
-	StackPtr_t stackPointer;
-	BasePtr_t basePointer;
-	TaskFlags_t flags;
+	StackPtr_t stackPointer;	/**< Saved SP when task is not running. */
+	BasePtr_t basePointer;		/**< Base of the task's stack. */
+	TaskFlags_t flags;			/**< Task flags. */
 } TCB_t;
 
+/**
+ * @brief Array of TCBs placed immediately before task stacks.
+ */
 typedef struct _TaskControlBlockArrayStruct
 {
-	size_t nTCBs;
-	TCB_t TCBs[];
+	size_t nTCBs;	/**< Number of valid TCB entries. */
+	TCB_t TCBs[];	/**< Flexible array of TCBs. */
 } TCBArray_t;
 
+/**
+ * @brief Static information block located at the top of RAM.
+ * 
+ * This structure is accessed via `__uRTOS_STATIC_INFO_PTR`. It contains
+ * kernel state that must survive across context switches.
+ */
 typedef struct _StaticInfoStruct
 {
-	TCB_t* current;
-	TCB_t* last;
+	TCB_t* current;			/**< Currently executing task. */
+	TCB_t* last;			/**< Last TCB in the array (cached, for boundary check). */
 
-	TCBArray_t* array;
+	TCBArray_t* array;		/**< Pointer to the TCB array. */
 
-	unsigned long tick;
+	unsigned long tick;		/**< System tick counter. */
 
-	Scheduler_t scheduler;
-	uint8_t sysFlags;
+	Scheduler_t scheduler;	/**< Current scheduler function. */
+	uint8_t sysFlags;		/**< System flags (timer ID, etc.). */
 } StaticInfo_t;
 
-
+/**
+ * @brief Second stage of system initialization.
+ * 
+ * Called after switching to the system stack. This function never returns.
+ * 
+ * @param initInfo Pointer to system initialization info (in PROGMEM).
+ * @param tasks Pointer to array of task descriptors (in PROGMEM).
+ * @param nTasks Number of tasks in the array.
+ */
 void uRTOS_Init(const SysInitInfo_t* initInfo, const TaskDesc_t* tasks, size_t nTasks) __attribute__((noreturn, noinline));
+
+/**
+ * @brief Timer tick handler (naked).
+ * 
+ * This function is jumped to from the user-installed ISR. It saves the
+ * current task context, invokes the scheduler on the system stack, and
+ * restores the context of the next task.
+ */
 void uRTOS_TickHandler(void);
 
 
